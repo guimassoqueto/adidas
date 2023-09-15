7868305975ba96d8b3b7e6320aad7289b66b721c
guimassoqueto <gui.massoqueto@gmail.com>
2023-09-15 05:54:05 -0300

    feat: add aiohttp, aiohttp-retry

---
+++ app/infra/aiohttp/helpers.py
@@ -0,0 +1,15 @@
+def header() -> dict:
+    return {
+        'Accept':	'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Encoding':	'gzip, deflate, br',
+        'Accept-Language':	'en-US,en;q=0.5',
+        'Connection':	'keep-alive',
+        'Host':	'www.adidas.com.br',
+        'Sec-Fetch-Dest':	'document',
+        'Sec-Fetch-Mode':	'navigate',
+        'Sec-Fetch-Site':	'none',
+        'Sec-Fetch-User':	'?1',
+        'TE':	'trailers',
+        'Upgrade-Insecure-Requests':	'1',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0'
+    }
