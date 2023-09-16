d2a31721a49975df2dc62d400db3137c5162ce6c
guimassoqueto <gui.massoqueto@gmail.com>
2023-09-16 08:26:03 -0300

    feat: add affiliate link

--- app/settings.py
+++ app/settings.py
@@ -8,5 +8,6 @@ POSTGRES_DB = getenv("POSTGRES_DB") or "postgres"
 POSTGRES_USER = getenv("POSTGRES_USER") or "postgres"
 POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD") or "password"
 POSTGRES_HOST = getenv("POSTGRES_HOST") or "127.0.0.1"
+TABLE_NAME = getenv("TABLE_NAME", "sports")
 ADIDAS_API = getenv("ADIDAS_API", "")
 MAX_CONCURRENCY = int(getenv("MAX_CONCURRENCY", 8))
